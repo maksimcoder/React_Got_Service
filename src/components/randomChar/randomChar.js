c803943a5d57f256bd9b04e1ee10175578555ac2
Max <vip.devyatilov@mailru>
2021-06-22 15:44:51 +0300

    Minor changes

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,33 +1,44 @@
 import React, {Component} from 'react';
 // import styled from 'styled-components';
+
 import {CharBlock, CharList, CharListItem} from '../charDetails/charDetails';
+import GotService from '../../services/gotService';
 import styles from './RandomChar.module.css';
 
 
 
 
 export default class RandomChar extends Component {
+    gotApi = new GotService();
+    state = {
+        name: null,
+        gender: null,
+        born: null,
+        died: null,
+        culture: null
+    }
 
     render() {
+        const {name, gender, born, died, culture} = this.state;
         return (
             <CharBlock>
-                <h4>Random Character: John</h4>
+                <h4>Random Character: {name}</h4>
                 <CharList flush>
                     <CharListItem>
                         <span className={styles.term}>Gender </span>
-                        <span>male</span>
+                        <span>{gender}</span>
                     </CharListItem>
                     <CharListItem>
                         <span className={styles.term}>Born </span>
-                        <span>11.03.1039</span>
+                        <span>{born}</span>
                     </CharListItem>
                     <CharListItem>
                         <span className={styles.term}>Died </span>
-                        <span>13.09.1089</span>
+                        <span>{died}</span>
                     </CharListItem>
                     <CharListItem>
                         <span className={styles.term}>Culture </span>
-                        <span>Anarchy</span>
+                        <span>{culture}</span>
                     </CharListItem>
                 </CharList>
             </CharBlock>
